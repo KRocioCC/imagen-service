c5a6d3bc21773cbb044726be256b0d14c8d61e91
Karen Calderon <krcatari@umsa.bo>
2026-06-26 23:56:40 -0400

    first commit

---
+++ src/main/java/com/ecommerce/imagen_service/ImagenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.imagen_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagenServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/imagen_service/ImagenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.imagen_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
